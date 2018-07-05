12b103af42fa23614376e0445fe7cdd531c6a8ba
astrolitterbox <sbekeraite@aip.de>
2018-07-05 23:51:42 +0300

    stop script

--- bigMotor.py
+++ bigMotor.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-
-
 #import chirp_modbus
 import minimalmodbus
 import time
@@ -21,23 +19,23 @@ GPIO.setup(rs2, GPIO.IN,  pull_up_down=GPIO.PUD_UP)
 GPIO.setup(rs3, GPIO.IN,  pull_up_down=GPIO.PUD_UP)    
 
 def first_switch(channel):
+    print("1")
     global clickCounter
     clickCounter+=1
-    print(clickCounter)  
 
 def second_switch(channel):
+    print("2")
     global clickCounter
     clickCounter+=1
-    print(clickCounter)    
 
 def third_switch(channel):
+    print("3")
     global clickCounter
     clickCounter+=1
-    print(clickCounter)    
 
-GPIO.add_event_detect(rs1, GPIO.FALLING, callback=first_switch, bouncetime=300)  
-GPIO.add_event_detect(rs2, GPIO.FALLING, callback=second_switch, bouncetime=300)  
-GPIO.add_event_detect(rs3, GPIO.FALLING, callback=third_switch, bouncetime=300)  
+GPIO.add_event_detect(rs1, GPIO.FALLING, callback=first_switch, bouncetime=150)  
+GPIO.add_event_detect(rs2, GPIO.FALLING, callback=second_switch, bouncetime=150)  
+GPIO.add_event_detect(rs3, GPIO.FALLING, callback=third_switch, bouncetime=150)  
 
 #minimalmodbus.TIMEOUT=0.5
 #minimalmodbus.CLOSE_PORT_AFTER_EACH_CALL = True
@@ -45,47 +43,54 @@ GPIO.add_event_detect(rs3, GPIO.FALLING, callback=third_switch, bouncetime=300)
 
 #MODBUS setup
 motor = minimalmodbus.Instrument('/dev/ttyUSB0', slaveaddress=ADDRESS)
-maxSpeed = 5*60
-accTime = 4*100
 
-motor.write_register(3, accTime, functioncode=6) #akseleravimo laikas, s*100
-motor.write_register(4, maxSpeed, functioncode=6, signed=True) #greitis
 
 #motor.precalculate_read_size=False
 motor.debug=True
+
+def setSpeed(hz):
+    maxSpeed = hz*60
+    motor.write_register(4, maxSpeed, functioncode=6, signed=True) #greitis
     
 
-#while True:
-def main():
-    print("start")
-    #motor.write_register(0, 0b0000000000000100, functioncode=6)
-    startTime = time.time()
-    #print(startTime)
+def start():
     motor.write_register(0, 0b0000000000000101, functioncode=6) #start
-    #start (101 fault reset, run)
+
+def stop():
+    motor.write_register(0, 0b0000000000000110, functioncode=6)#stop#stop
+
+def setAcc(s):
+    accTime = s*100
+    motor.write_register(3, accTime, functioncode=6) #akseleravimo laikas, s*100
+
+def rotate(clickLimit):
+    global clickCounter
+    clickCounter = 0
+    start()
+    print("start")
     while True:
-        #if (clickCounter == 100):
-         #   print("stop")
-        s = time.time() - startTime
-        print(s)
         print(clickCounter)
-        if s > 5:
-            print(clickCounter)
-            #motor.write_register(3, accTime, functioncode=6) #akseleravimo laikas, s*100
-            motor.write_register(0, 0b0000000000000110, functioncode=6)#stop#stop
-            print(clickCounter)
-            
-            break
+        if (clickCounter > clickLimit):
+               print(clickCounter)
+               stop()
+               time.sleep(2)
+               break
+    
+
+def main():
+    speed = 5
+    setAcc(2) #seconds
+    clickLimit = 100
+    #while True:
+    for i in range(0, 25):
+            setSpeed(speed) #Hz
+            print("fwd")
+            rotate(clickLimit)
+            setSpeed(-speed) #Hz
+            print("back", clickCounter)
+            rotate(clickLimit)  
+                
     GPIO.cleanup() # this ensures a clean exit  
-    #sleep(100)
-    #rotate(fwd)
-    #readSwitch(rs3)
-    #sleep(sleepValue)
-    #rotate(-1*fwd)
-    #print("finished")
-    #motor.write_register(0, 0b0000000000000001, functioncode=6)#stop
-
-
 
 if __name__ == "__main__":
       main()
---
+++ stop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#import chirp_modbus
+import minimalmodbus
+minimalmodbus.BAUDRATE = 9600
+ADDRESS = 1
+#MODBUS setup
+motor = minimalmodbus.Instrument('/dev/ttyUSB0', slaveaddress=ADDRESS)
+
+
+#motor.precalculate_read_size=False
+motor.debug=True
+motor.write_register(0, 0b0000000000000110, functioncode=6)#stop#stop
